5efff4455427e47daecb62e05b906aede0510655
Eduardo <ejrb1234@gmail.com>
2026-01-22 22:29:28 -0400

    add TanStackQuery

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './auth/AuthProvider';
 import { SidebarProvider } from './ui/sidebar';
 import { Toaster } from './ui/sonner';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import QueryProvider from './providers/query-provider';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -14,18 +15,20 @@ interface ProvidersProps {
 
 export const Providers = ({ children, sidebarIsOpen }: ProvidersProps) => {
   return (
-    <AuthProvider>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <SidebarProvider defaultOpen={sidebarIsOpen}>
-          <>{children}</>
-        </SidebarProvider>
-      </NextThemesProvider>
-      <Toaster />
-    </AuthProvider>
+    <QueryProvider>
+      <AuthProvider>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SidebarProvider defaultOpen={sidebarIsOpen}>
+            <>{children}</>
+          </SidebarProvider>
+        </NextThemesProvider>
+        <Toaster />
+      </AuthProvider>
+    </QueryProvider>
   );
 };
