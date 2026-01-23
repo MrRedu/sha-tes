34b9c250e4560dc0fed6175464a9c876182ff8ee
Eduardo <ejrb1234@gmail.com>
2026-01-22 23:32:38 -0400

    delete util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatCode(code: string): string {
-  const firstPart = code.slice(0, 4);
-  const secondPart = code.slice(4);
-
-  return `${firstPart}-${secondPart}`;
-}
